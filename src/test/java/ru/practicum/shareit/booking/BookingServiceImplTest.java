c68f507c9d3e3255a870af6537d778aef4dee7b0
Mihail Krasnogorskiy <mikser256@yandex.ru>
2022-09-07 22:44:02 +0300

    начата работа над BookingServiceImplTest

---
+++ src/test/java/ru/practicum/shareit/booking/BookingServiceImplTest.java
@@ -0,0 +1,155 @@
+package ru.practicum.shareit.booking;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import ru.practicum.shareit.booking.dto.BookingDto;
+import ru.practicum.shareit.booking.dto.CreatingBookingDto;
+import ru.practicum.shareit.booking.model.BookingStatus;
+import ru.practicum.shareit.booking.service.BookingService;
+import ru.practicum.shareit.exception.BookingApprovedException;
+import ru.practicum.shareit.exception.EndDateValidateException;
+import ru.practicum.shareit.exception.NotFoundException;
+import ru.practicum.shareit.exception.OwnerValidationException;
+import ru.practicum.shareit.item.dto.ItemDto;
+import ru.practicum.shareit.item.service.ItemService;
+import ru.practicum.shareit.user.dto.UserDto;
+import ru.practicum.shareit.user.service.UserService;
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class BookingServiceImplTest {
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private ItemService itemService;
+
+    @Autowired
+    private BookingService bookingService;
+    LocalDateTime start = LocalDateTime.now().withNano(0).plusDays(1);
+    LocalDateTime end = start.plusDays(1);
+
+    private CreatingBookingDto creatingDto = CreatingBookingDto.builder()
+            .booker(2L)
+            .itemId(1L)
+            .start(start)
+            .end(end)
+            .build();
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Test
+    void test21_create() {
+        BookingDto dto = bookingService.create(2L, creatingDto);
+        assertEquals(1, dto.getId());
+        assertEquals(start, dto.getStart());
+        assertEquals(end, dto.getEnd());
+        assertEquals(1, dto.getItem().getId());
+        assertEquals(1, dto.getItem().getOwner());
+        assertEquals(2, dto.getBooker().getId());
+        assertEquals(BookingStatus.WAITING, dto.getStatus());
+    }
+
+    @Test
+    void test22_approve() {
+        Throwable thrown = assertThrows(NotFoundException.class, () -> {
+            bookingService.approve(1, false, 1);
+        });
+        assertEquals("This booking not found", thrown.getMessage());
+        bookingService.create(2, creatingDto);
+        BookingDto dto = bookingService.approve(1, true, 1);
+        assertEquals(1, dto.getId());
+        assertEquals(start, dto.getStart());
+        assertEquals(end, dto.getEnd());
+        assertEquals(1, dto.getItem().getId());
+        assertEquals(1, dto.getItem().getOwner());
+        assertEquals(2, dto.getBooker().getId());
+        assertEquals(BookingStatus.APPROVED, dto.getStatus());
+        thrown = assertThrows(BookingApprovedException.class, () -> {
+            bookingService.approve(1, false, 1);
+        });
+        assertEquals("You have already replied to this booking", thrown.getMessage());
+        thrown = assertThrows(OwnerValidationException.class, () -> {
+            bookingService.approve(2, false, 1);
+        });
+        assertEquals("You are not item owner", thrown.getMessage());
+        thrown = assertThrows(NotFoundException.class, () -> {
+            bookingService.approve(3, false, 1);
+        });
+        assertNotNull(thrown.getMessage());
+        clearEnvironment();
+        createEnvironment();
+        bookingService.create(2, creatingDto);
+        dto = bookingService.approve(1, false, 1);
+        assertEquals(1, dto.getId());
+        assertEquals(start, dto.getStart());
+        assertEquals(end, dto.getEnd());
+        assertEquals(1, dto.getItem().getId());
+        assertEquals(1, dto.getItem().getOwner());
+        assertEquals(2, dto.getBooker().getId());
+        assertEquals(BookingStatus.REJECTED, dto.getStatus());
+    }
+
+    @Test
+    void test23_validationEndDate() {
+        creatingDto.setEnd(start);
+        creatingDto.setStart(end);
+        Throwable thrown = assertThrows(EndDateValidateException.class, () -> {
+            bookingService.create(2, creatingDto);
+        });
+        assertEquals("EndDate is before StartDate", thrown.getMessage());
+        creatingDto.setStart(start);
+        creatingDto.setEnd(end);
+    }
+
+    @BeforeEach
+    void createEnvironment() {
+        UserDto user = UserDto.builder()
+                .name("Voldemar")
+                .email("voldemar@mail.ru")
+                .build();
+        userService.create(user);
+        user.setName("Ivan");
+        user.setEmail("ivan@ivan.ru");
+        userService.create(user);
+        ItemDto itemDto = ItemDto.builder()
+                .name("Машина")
+                .description("Audi TT")
+                .available(true)
+                .owner(1)
+                .comments(new HashSet<>())
+                .build();
+        itemService.create(1L, itemDto);
+    }
+
+    @AfterEach
+    void clearEnvironment() {
+        String query = "SET REFERENTIAL_INTEGRITY = FALSE";
+        jdbcTemplate.update(query);
+        query = "TRUNCATE TABLE users ";
+        jdbcTemplate.update(query);
+        query = "ALTER TABLE users ALTER COLUMN id RESTART WITH 1";
+        jdbcTemplate.update(query);
+        query = "TRUNCATE TABLE items ";
+        jdbcTemplate.update(query);
+        query = "ALTER TABLE items ALTER COLUMN id RESTART WITH 1";
+        jdbcTemplate.update(query);
+        query = "TRUNCATE TABLE bookings ";
+        jdbcTemplate.update(query);
+        query = "ALTER TABLE bookings ALTER COLUMN id RESTART WITH 1";
+        jdbcTemplate.update(query);
+        query = "SET REFERENTIAL_INTEGRITY = TRUE";
+        jdbcTemplate.update(query);
+    }
+
+}
